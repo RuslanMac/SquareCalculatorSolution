f8bf759938582dd298ad1c1c51bf3200e7e8afd1
Ruslan Karamurzanov <ruslan-karamurzanov@mail.ru>
2023-08-27 18:54:03 +0600

    add Validation for sequence class

---
+++ SquareCalculatorSolution.Application/Validator/SequenceDtoValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.Options;
+using SquareCalculatorSolution.Application.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareCalculatorSolution.Application.Validator
+{
+    public class SequenceDtoValidator : AbstractValidator<SequenceDto>
+    {
+        private readonly SequenceOptions _optionsValue; 
+
+        public SequenceDtoValidator(IOptions<SequenceOptions> options)
+        {
+            _optionsValue = options.Value;
+            RuleFor(s => s.Numbers.Length).NotNull().NotEmpty().LessThanOrEqualTo(_optionsValue.MaxNumber);
+            RuleForEach(s => s.Numbers).LessThanOrEqualTo(_optionsValue.MaxValue);
+            RuleForEach(s => s.Numbers).GreaterThanOrEqualTo(_optionsValue.MinValue);
+
+        }
+    }
+}
